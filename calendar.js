af4e70dc5c07a2421cf7d85455f10a9e059669c2
Maya Mohsin Ahmed <maya.ahmed@gmail.com>
2017-01-01 23:04:55 +0530

    Add files via upload

--- calendar.js
+++ calendar.js
@@ -1,6 +1,9 @@
+  var d = new Date();
 window.onload = function(){
-  //  var d = new Date();
-var d = new Date(new Date().getTime()+24*60*60*1000);
+  run();
+}
+
+function run(){
     var month_name = ['January','February','March','April','May','June','July','August','September','October','November','December'];
     var month = d.getMonth();   //0-11
     var year = d.getFullYear(); //2017
@@ -15,24 +18,68 @@ var d = new Date(new Date().getTime()+24*60*60*1000);
     //Tue Sep 30 2017 ...
     var calendar = get_calendar(day_no, days);
     document.getElementById("calendar-month-year").innerHTML = month_name[month]+" "+year;
+document.getElementById("calendar-date").innerHTML="";
     document.getElementById("calendar-date").appendChild(calendar);
 }
 
+function getNumberOfDays(){
+var n = d.getDate();
+if(n < 15) return 35;
+if(n > 14) return 20;
+}
+function getPreviousNumberOfDays(){
+var n = d.getDate();
+if(n < 15) return 20;
+if(n > 14) return 35;
+}
+
+function nextMonth(){
+var days = getNumberOfDays();
+ d.setDate(d.getDate()+days);
+run();
+}
+
+function previousMonth(){
+var days = getPreviousNumberOfDays();
+ d.setDate(d.getDate()-days);
+run();
+}
+
+
 function get_calendar(day_no, days){
     var table = document.createElement('table');
+table.style.border="1px solid beige";
     var tr = document.createElement('tr');
     
-    //row for the day letters
-    for(var c=0; c<=6; c++){
+tr.style.color="khaki";
+//row for the day letters
+   
         var td = document.createElement('td');
-        td.innerHTML = "SMTWTFS"[c];
+        td.innerHTML = "Su";
+    tr.appendChild(td);
+  var td = document.createElement('td');
+ td.innerHTML = "Mo";
+    tr.appendChild(td);
+var td = document.createElement('td');
+ td.innerHTML = "Tu";
+    tr.appendChild(td);
+  var td = document.createElement('td');
+ td.innerHTML = "We";
+    tr.appendChild(td);var td = document.createElement('td');
+ td.innerHTML = "Th";
+    tr.appendChild(td);var td = document.createElement('td');
+ td.innerHTML = "Fr";
+    tr.appendChild(td);var td = document.createElement('td');
+ td.innerHTML = "Sa";
+    tr.appendChild(td);  
 
-        tr.appendChild(td);
-    }
+
     table.appendChild(tr);
     
     //create 2nd row
     tr = document.createElement('tr');
+
+
     var c;
     for(c=0; c<=6; c++){
         if(c == day_no){
